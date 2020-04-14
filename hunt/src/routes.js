a39239578146b8ed4fe80d2fbb8818b4bb1e0383
Wallace <wbescrovaine@gmail.com>
2020-04-14 14:56:22 -0300

    Detalhe com WebView

--- hunt/src/routes.js
+++ hunt/src/routes.js
@@ -1,7 +1,17 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from "react-navigation";
 
 import Main from "./pages/main";
+import Product from "./pages/product";
 
 export default createStackNavigator({
     Main,
-});
+    Product
+},
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#DA552F"
+            },
+            headerTintColor: "#FFF"
+        },
+    });
