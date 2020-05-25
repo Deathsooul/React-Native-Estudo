bf44a716842d8d23adc5a8e0b1d1f14444d3675f
Wallace <wbescrovaine@gmail.com>
2020-05-25 08:30:42 -0300

    Tela inicial Be The Hero

---
+++ bethehero/src/pages/Incidents/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
+
+export default function Incidents() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                </Text>
+            </View>
+
+
+            <Text style={styles.title}> Bem vindo !</Text>
+            <Text style={styles.description}> Escolha um dos casos abaixo e salve uma vida</Text>
+
+
+            <View style={styles.incidents}>
+                <View style={styles.incident}>
+                    <Text style={styles.incidentPropoerty}>ONG:</Text>
+                    <Text style={styles.incidentValue}>APAD</Text>
+
+                    <Text style={styles.incidentPropoerty}>CASO:</Text>
+                    <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                    <Text style={styles.incidentPropoerty}>VALOR:</Text>
+                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+                </View>
+            </View>
+
+        </View>
+    );
+}
+
+
